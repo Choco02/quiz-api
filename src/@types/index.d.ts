443195b404274b4c94537e003cde1ab138fb59ab
Choco02 <sugarzx01@gmail.com>
2022-10-06 17:07:24 -0300

    feat: reorganizando as interfaces de Quiz

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -13,3 +13,29 @@ interface RegisterData {
     name?: string;
     role?: string;
 }
+
+interface QuizData {
+    question: string;
+    category: string;
+    answer1: {
+        content: string;
+        right: boolean;
+    };
+    answer2: {
+        content: string;
+        right: boolean;
+    };
+    answer3: {
+        content: string;
+        right: boolean;
+    }
+}
+
+interface Quiz {
+    questions: QuizData[]
+}
+
+interface ValidatedReturn {
+    validated: boolean,
+    err: string
+}
