ee4c099a226a1c644e619a243eddd85a2c49e28e
Choco02 <sugarzx01@gmail.com>
2022-10-07 09:47:02 -0300

    feat: parsers de questões

--- src/util/index.ts
+++ src/util/index.ts
@@ -10,3 +10,95 @@ export const generatePasswordHash = (password: string): string => {
     return hash;
 
 };
+
+export const validateQuestions = (data: Quiz) => {
+
+    for (const question of data.questions) {
+
+        if (!question.question) {
+            return { validated: false, err: 'Question is missing' };
+        }
+
+        if (!question.category) {
+            return { validated: false, err: 'Category is missing' };
+        }
+
+        const answers: string[] = Object.keys(question)
+            .filter(i => i.includes('answer'));
+
+        const filterQuestions = (option: boolean) => {
+            return answers.filter(
+                // @ts-ignore
+                (a: string) => question[a].right === option);
+        };
+
+        const correctAnswers = filterQuestions(true);
+
+        const wrongAnswers = filterQuestions(false);
+
+        if (correctAnswers.length > 1) {
+            return { validated: false, err: `Only one question can be correct at questions[${data.questions.indexOf(question)}]` };
+        }
+
+        if (wrongAnswers.length > 2) {
+            return { validated: false, err: `No question is correct at questions[${data.questions.indexOf(question)}]` };
+        }
+
+    }
+
+    return { validated: true, err: '' };
+
+};
+
+export const objToJson = (data: Quiz) => {
+
+    for (const question of data.questions) {
+
+        const answers = Object.keys(question).filter(i => i.includes('answer'));
+
+        for (const answerKey of answers) {
+
+            // @ts-ignore
+            question[answerKey] = JSON.stringify(question[answerKey]);
+
+        }
+
+    }
+
+    return data;
+};
+
+export const jsonToObj = (data: Quiz) => {
+
+    for (const question of data.questions) {
+
+        const answers = Object.keys(question).filter(i => i.includes('answer'));
+
+        for (const answerKey of answers) {
+
+            // @ts-ignore
+            question[answerKey] = JSON.parse(question[answerKey]);
+
+        }
+
+    }
+
+    return data;
+};
+
+export const hideCorrectAnswers = (data: Quiz) => {
+
+    for (const question of data.questions) {
+
+        const answers = Object.keys(question).filter(i => i.includes('answer'));
+
+        for (const answerKey of answers) {
+
+            // @ts-ignore
+            delete question[answerKey].right;
+        }
+
+    }
+
+    return data;
+};
