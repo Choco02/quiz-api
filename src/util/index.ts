b280323523e20a2933405cc8e24a80b46c25ce80
Choco02 <sugarzx01@gmail.com>
2022-10-09 08:43:47 -0300

    feat: função de shuffle para enviar questões diferentes

--- src/util/index.ts
+++ src/util/index.ts
@@ -106,3 +106,19 @@ export const hideCorrectAnswers = (data: Quiz) => {
 
     return data;
 };
+
+export const shuffle = (array: QuizData[]) => {
+
+    let currentIndex = array.length, randomIndex;
+
+    while (currentIndex != 0) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+};
