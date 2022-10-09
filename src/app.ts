8a7a66753f6844cac0ffe5b92a074804b4416c27
Choco02 <sugarzx01@gmail.com>
2022-10-09 10:18:34 -0300

    feat(logger): adicionar logger simples

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import { router } from './routes';
 
+import pino from 'pino-http';
+const logger = pino();
+
 const app = express();
 
+app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
