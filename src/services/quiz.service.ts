e7f11a31ad3807123ec89ef0bc027f337063f3c5
Choco02 <sugarzx01@gmail.com>
2022-10-13 11:43:25 -0300

    feat(service): método reply do service quiz

--- src/services/quiz.service.ts
+++ src/services/quiz.service.ts
@@ -34,4 +34,52 @@ export class QuizService {
 
         return data;
     }
+
+    async reply(questions: QuizReplyData[]) {
+
+        const questionIds = questions.map(q => q.questionId);
+
+        const allQuestions = await prisma.quiz.findMany({
+            where: {
+                id: {
+                    in: questionIds
+                }
+            }
+        });
+
+        jsonToObj({ questions: allQuestions as unknown as QuizData[] });
+
+        let points = 0;
+        let right = 0;
+        let wrong = 0;
+
+        for (const id of questionIds) {
+
+            const questionDb = allQuestions.find(i => i.id === id) as unknown as QuizData;
+
+            const answerKeys = Object.keys(questionDb)
+                .filter(i => i.includes('answer'))
+                .sort() as ('answer1'|'answer2'|'answer3')[];
+
+            const questionReceived = questions
+                .find(i => i.questionId === id) as unknown as QuizReplyData;
+
+            const questionReplied = answerKeys[
+                Number(questionReceived.questionReplied) - 1
+            ];
+
+            if (questionDb[questionReplied].right) {
+
+                points++;
+                right++;
+            }
+            else {
+                if (points > 0) points--;
+                wrong++;
+            }
+        }
+
+        return { wrong, right, points };
+
+    }
 }
